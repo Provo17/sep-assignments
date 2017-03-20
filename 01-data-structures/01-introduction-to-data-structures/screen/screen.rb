4b4e953d0c06499655be8dba1c63e762d0d96b81
provo17 <provo17.gp@gmail.com>
2017-03-20 19:31:57 +0000

    all tests passing

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -1,23 +1,41 @@
 require_relative 'pixel'
 
+
 class Screen
   attr_accessor :width
   attr_accessor :height
   attr_accessor :matrix
 
   def initialize(width, height)
+    @width = width
+    @height = height
+    @matrix = Array.new(width) {Array.new(height)}
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    if inbounds(x,y)
+      @matrix[x][y] = pixel
+      return @matrix[x][y]
+    end
   end
 
   def at(x, y)
+    if inbounds(x, y)
+      return @matrix[x][y]
+    end
   end
 
   private
 
   def inbounds(x, y)
+    if (x < 0) || (x > width - 1) 
+      puts " is out of bounds"
+    elsif (y < 0) || (y > height - 1)  
+      puts "is out of bounds"
+    else
+      puts "is inbounds"
+      return true
+    end
   end
-
 end
