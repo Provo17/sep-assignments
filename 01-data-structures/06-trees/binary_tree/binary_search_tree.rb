3ea75f6c9a4b00848660e68c24c09030ebfc3f1c
provo17 <provo17.gp@gmail.com>
2017-05-08 23:48:01 +0000

    assignment complete

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -20,17 +20,40 @@ class BinarySearchTree
 
   # Recursive Depth First Search
   def find(root, data)
-    if root.nil? || data.nil?
-      return nil
-    else 
-    
+    return nil if data.nil? || root.nil?
+    if  root.title == data
+      return root
+    elsif root.right != nil
+      find( root.right, data)
+    elsif root.left != nil
+      find(root.left, data)
     end
   end
 
   def delete(root, data)
+    return nil if data.nil? || root.nil?
+    node = find(root, data)
+    if node.nil? 
+      nil
+    else
+      node.title = nil && node.rating = nil
+    end
   end
 
   # Recursive Breadth First Search
   def printf(children=nil)
+    start = [@root]
+    list = []
+    while start.length > 0
+      new_root = start.shift
+      if new_root.left != nil
+        start.push(new_root.left)
+      end
+      if new_root.right != nil
+        start.push(new_root.right)
+      end
+      list.push("#{new_root.title}: #{new_root.rating}")
+    end
+    list.each {|x| puts x}
   end
 end
