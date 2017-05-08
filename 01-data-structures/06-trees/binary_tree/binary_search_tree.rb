81ad2c9a57f53514d77b3904afed57db6359cea0
provo17 <provo17.gp@gmail.com>
2017-05-08 05:02:53 +0000

    binary tree in progress

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -3,13 +3,28 @@ require_relative 'node'
 class BinarySearchTree
 
   def initialize(root)
+    @root = root
   end
 
   def insert(root, node)
+    if root.rating > node.rating
+      if root.left.nil? 
+        root.left = node
+      else
+        insert(root.left, node)
+      end
+    else
+      root.right.nil? ? root.right = node : insert(root.right, node) # New trick!!! The ?: operator acts as a one line if/then/else statement.
+    end
   end
 
   # Recursive Depth First Search
   def find(root, data)
+    if root.nil? || data.nil?
+      return nil
+    else 
+    
+    end
   end
 
   def delete(root, data)
